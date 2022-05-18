34d1c57cc0d3a98c61f30ac314ad9976d619aad7
Shiva <shiva@Englands-MacBook-Pro.local>
2022-05-18 12:20:13 -0400

    git repo for react projects

---
+++ myapp2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Comp1, Comp2 } from './components/comp1';
+
+function App() {
+  return (
+    <>
+      <Comp1 />
+      <Comp2 />
+    </>
+  );
+}
+
+export default App;
---
+++ myapp2/src/components/comp1.js
@@ -0,0 +1,19 @@
+const Comp1 = () => {
+  return (
+    <>
+      <p>React DOM</p>
+      <h1>comp1 h1</h1>
+    </>
+  )
+};
+
+const Comp2 = () => {
+  return (
+    <>
+      <h2> comp2 h2 </h2>
+    </>
+  )
+}
+
+// export default Comp1;
+export { Comp1, Comp2 };
